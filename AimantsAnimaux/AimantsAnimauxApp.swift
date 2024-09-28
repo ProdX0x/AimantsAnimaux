2a0648e46adeaf4bdfa60ea400e4f428ef462b73
ProdX0x <ss_saulnier@icloud.com>
2024-09-28 18:04:32 +0200

    Ajout de tous les fichiers du projet AimantsAnimaux

---
+++ AimantsAnimaux/AimantsAnimauxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AimantsAnimauxApp.swift
+//  AimantsAnimaux
+//
+//  Created by Steve on 25/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct AimantsAnimauxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AnimalsListView()
+        }
+    }
+}
---
+++ AimantsAnimaux/AnimalsListView.swift
@@ -0,0 +1,74 @@
+//
+//  AnimalsListView.swift
+//  AimantsAnimaux
+//
+//  Created by Steve on 25/09/2024.
+//
+
+import SwiftUI
+///  ------- Vues ---------
+
+// Affiche la liste des animaux
+struct AnimalsListView: View {
+    @State private var showingAdd = false
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Image("aimantsDanimauxBan")
+                    .resizable()
+                    .background(.clear)
+                    .frame(height: 130)
+                    .aspectRatio(contentMode: .fit)
+                    
+                
+                List(animalList, id: \.name) { animal in
+                    NavigationLink {
+                        AnimalDetailView(animal: animal)
+                    } label: {
+                        AnimalRow(animal: animal)
+                    }
+                }
+                .navigationTitle("Liste des animaux")
+                .toolbar {
+                    ToolbarItemGroup(placement: .navigationBarTrailing) {
+                        NavigationLink {
+                            AddNewAnimalView()
+                        } label : {
+                            Text("Ajouter")
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+
+
+    // Represente la vue d'une ligne de la liste
+    struct AnimalRow: View {
+        let animal: Animal
+        
+        var body: some View {
+            HStack {
+                Image(systemName: getImageName(breed: animal.breed))
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .foregroundColor(Color.green)
+                    .frame(width: 40, height: 40)
+                    .padding(8)
+                    .clipShape(Circle())
+                    .overlay {
+                        Circle().stroke(.green, lineWidth: 3)
+                    }
+                
+                Text(animal.name)
+                    .bold()
+                Spacer()
+            }
+        }
+    }
+}
+#Preview {
+    AnimalsListView()
+}
